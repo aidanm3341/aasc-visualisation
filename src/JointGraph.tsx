4046610532a9c2eccf0610c694ae80d92f8eecc2
Aidan McPhelim <aidanmcp@hotmail.co.uk>
2024-02-21 16:10:39 +0000

    refactor of graph component

--- src/JointGraph.tsx
+++ src/JointGraph.tsx
@@ -8,11 +8,90 @@ interface Props {
     relationships: Relationship[]
 }
 
-export function JointGraph({ nodes, relationships }: Props) {
+const rects: {[name: string]: joint.shapes.standard.Rectangle} = {}
 
+let recentRect: joint.shapes.standard.Rectangle;
+
+function createRelationships(graph: joint.dia.Graph, relationships: Relationship[]) {
+    relationships.forEach(relationship => {
+        const link = new joint.shapes.standard.Link();
+        link.appendLabel({
+            attrs: {
+                text: {
+                    text: relationship.relationshipType
+                }
+            }
+        });
+
+        link.source(rects[relationship.source]);
+        link.target(rects[relationship.target]);
+        link.addTo(graph);
+    });
+}
+
+function createNodes(graph: joint.dia.Graph, nodes: Node[]) {
+    nodes.forEach(node => {
+        const rect = new joint.shapes.standard.Rectangle();
+        rect.resize(200, 40);
+        rect.attr({
+            body: {
+                fill: '#44CCFF'
+            },
+            label: {
+                text: node.name,
+                fill: 'black'
+            }
+        });
+
+        rect.prop('extra', node.extras);
+        rect.addTo(graph);
+        rects[node.uniqueId] = rect;
+    })
+}
+
+function createClickEvents(paper: joint.dia.Paper, graph: joint.dia.Graph) {
+    paper.on('cell:pointerdown', (cellView, _evt, x, y) => {
+        if (recentRect)
+            recentRect.remove();
+
+        const rect = new joint.shapes.standard.Rectangle();
+        rect.resize(200, 40);
+        rect.position(x, y);
+
+        rect.attr({
+            body: {
+                fill: 'white'
+            },
+            label: {
+                text: JSON.stringify(cellView.model.attributes['extra'], null, 2),
+                fill: 'black',
+                'text-anchor': 'left',
+                'x-alignment': 'left'
+            }
+        });
+        rect.addTo(graph);
+        rect.resize(300, 200);
+        recentRect = rect;
+    });
+
+    paper.on('blank:pointerdown', () => {
+        recentRect.remove();
+    });
+}
+
+function applyLayout(graph: joint.dia.Graph) {
+    DirectedGraph.layout(graph, {
+        nodeSep: 150,
+        edgeSep: 180,
+        rankDir: "TB",
+        marginX: 300,
+        marginY: 200
+    });
+}
+
+export function JointGraph({ nodes, relationships }: Props) {
     useEffect(() => {
         const namespace = joint.shapes;
-
         const graph = new joint.dia.Graph({}, { cellNamespace: namespace });
         
         const paper = new joint.dia.Paper({
@@ -26,79 +105,11 @@ export function JointGraph({ nodes, relationships }: Props) {
                 color: 'white'
             }
         });
-
-        let recentRect: joint.shapes.standard.Rectangle;
-        paper.on('cell:pointerdown', (cellView, _evt, x, y) => {
-            if (recentRect)
-                recentRect.remove();
-
-            const rect = new joint.shapes.standard.Rectangle();
-            rect.resize(200, 40);
-            rect.position(x, y);
-
-            rect.attr({
-                body: {
-                    fill: 'white'
-                },
-                label: {
-                    text: JSON.stringify(cellView.model.attributes['extra'], null, 2),
-                    fill: 'black',
-                    'text-anchor': 'left',
-                    'x-alignment': 'left'
-                }
-            });
-            rect.addTo(graph);
-            rect.resize(300, 200);
-            recentRect = rect;
-        });
-
-        paper.on('blank:pointerdown', () => {
-            recentRect.remove();
-        });
-
-        const rects: {[name: string]: joint.shapes.standard.Rectangle} = {}
         
-        nodes.forEach(node => {
-            const rect = new joint.shapes.standard.Rectangle();
-            rect.resize(200, 40);
-            rect.attr({
-                body: {
-                    fill: '#44CCFF'
-                },
-                label: {
-                    text: node.name,
-                    fill: 'black'
-                }
-            });
-
-            rect.prop('extra', node.extras);
-            rect.addTo(graph);
-            rects[node.uniqueId] = rect;
-        })
-        
-        relationships.forEach(relationship => {
-            const link = new joint.shapes.standard.Link();
-            link.appendLabel({
-                attrs: {
-                    text: {
-                        text: relationship.relationshipType
-                    }
-                }
-            });
-
-            link.source(rects[relationship.source]);
-            link.target(rects[relationship.target]);
-            link.addTo(graph);
-        });
-        
-
-        DirectedGraph.layout(graph, {
-            nodeSep: 150,
-            edgeSep: 180,
-            rankDir: "TB",
-            marginX: 300,
-            marginY: 200
-        });
+        createClickEvents(paper, graph);
+        createNodes(graph, nodes);
+        createRelationships(graph, relationships);
+        applyLayout(graph);
     })
 
     return (
